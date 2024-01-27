8dcdcb8dd5c8c38f8d4bf919e233ddcb9f4a477b
Evan Hubinger <evanjhub@gmail.com>
2024-01-26 21:33:56 -0800

    Add py_bytes

--- sublime_coconut/specification.py
+++ sublime_coconut/specification.py
@@ -223,7 +223,7 @@ syntax['repository'].update({
             'match':
             (
                 r'(?<!(?<!\.)\.)\b('
-                r'bool|bytearray|bytes|classmethod|complex|dict|(py_)?range|tuple|(py_)?open|'
+                r'bool|bytearray|(py_)?bytes|classmethod|complex|dict|(py_)?range|tuple|(py_)?open|'
                 r'(py_)?enumerate|float|frozenset|(py_)?int|list|memoryview|(py_)?object|'
                 r'property|(py_)?reversed|set|slice|staticmethod|(py_)?str|(py_)?super|type|'
                 r'(py_)?map|(py_)?zip|(process|thread|async)_map|count|starmap|zip_longest|'
